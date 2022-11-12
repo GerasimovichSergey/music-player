ac0e016db80dfbd984de6762345a1fbfe55f75de
Gerasimovich Sergey <wmmoee@gmail.com>
2022-11-13 00:32:15 +0300

    feat: add stop фсешщт

---
+++ js/index.js
@@ -0,0 +1,45 @@
+const audio = new Audio();
+const tracksCard = document.getElementsByClassName('track');
+const player = document.querySelector('.player');
+const pauseBtn = document.querySelector('.player__controller-pause');
+const stopBtn = document.querySelector('.player__controller-stop');
+
+function playMusic(event) {
+  const trackActive = event.currentTarget;
+
+  audio.src = trackActive.dataset.track;
+  audio.play();
+  pauseBtn.classList.remove('player__icon_play');
+  player.classList.add('player_active');
+
+  for (let i = 0; i < tracksCard.length; i++) {
+    tracksCard[i].classList.remove('track_active');
+  }
+
+  trackActive.classList.add('track_active');
+}
+
+function stopMusic() {
+  audio.src = '';
+}
+
+for (let i = 0; i < tracksCard.length; i++) {
+  tracksCard[i].addEventListener('click', playMusic);
+}
+
+pauseBtn.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+    pauseBtn.classList.remove('player__icon_play');
+  } else {
+    audio.pause();
+    pauseBtn.classList.add('player__icon_play');
+  }
+});
+
+stopBtn.addEventListener('click', () => {
+  if (audio.played) {
+    stopMusic();
+    player.classList.remove('player_active');
+  }
+});
